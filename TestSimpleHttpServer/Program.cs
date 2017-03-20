3024cd4d756e1722569c8c2d9417e57c6f8f59c8
makandat <tadnakam@outlook.jp>
2017-03-20 16:46:07 +0900

    SimpleHttpServer
    Test of  SimpleHttpServer

---
+++ TestSimpleHttpServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using SimpleHttpServer;
+using System;
+using System.Threading;
+
+namespace TestSimpleHttpServer
+{
+    /// <summary>
+    /// メイン
+    /// </summary>
+    /// <remarks>bin\Release (or bin\Debug) に Resources\Pages\404.html, 500.html を作成しておかないとエラーになる。</remarks>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int PORT = 5411;
+            Console.WriteLine($"開始 SimpleHttpServer port={PORT} ...");
+            try
+            {
+                HttpServer httpServer = new HttpServer(PORT, Routes.GET);
+
+                Thread thread = new Thread(new ThreadStart(httpServer.Listen));
+                thread.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("バックグランドで動作中。");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestSimpleHttpServer/Routes.cs
@@ -0,0 +1,43 @@
+﻿using SimpleHttpServer.Models;
+using SimpleHttpServer.RouteHandlers;
+using System.Collections.Generic;
+
+namespace TestSimpleHttpServer
+{
+    static class Routes
+    {
+        public static List<Route> GET
+        {
+            get
+            {
+                return new List<Route>()
+                {
+                    new Route()
+                    {
+                        Callable = HomeIndex,
+                        UrlRegex = "^\\/$",
+                        Method = "GET"
+                    },
+                    new Route()
+                    {
+                        Callable = new FileSystemRouteHandler() { BasePath = @"C:\Users\Barend.Erasmus\Desktop\Test"}.Handle,
+                        UrlRegex = "^\\/Static\\/(.*)$",
+                        Method = "GET"
+                    }
+                };
+
+            }
+        }
+
+        private static HttpResponse HomeIndex(HttpRequest request)
+        {
+            return new HttpResponse()
+            {
+                ContentAsUTF8 = "Hello",
+                ReasonPhrase = "OK",
+                StatusCode = "200"
+            };
+
+        }
+    }
+}
